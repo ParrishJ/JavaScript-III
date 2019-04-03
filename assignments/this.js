b69f882963a03f28fbab01cad10f6adff94cd2ce
ParrishJ <jarparrish@gmail.com>
2019-04-03 12:56:24 -0700

    Started this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this" - Jared-Parrish;
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When implicit binding is at work, one uses dot notation to invoke a function.  When doing this, one can look to the left of the dot to see what the "this" is referring to.
+* 2. Explicit binding using keywords such as call, apply, and bind to explicitly tell the function where "this" is referring to.
+* 3. When "new" binding is at work, one must use a class constructor to create a template object that uses "this" keyword to bind to new objects that are created using that class constructor.  
+* 4. Window binding refers to the behavior of the "this" keyword to default to refering to everything that is operating in the browser window when it is invoked on a global scope.
 *
 * write out a code example of each explanation above
 */
